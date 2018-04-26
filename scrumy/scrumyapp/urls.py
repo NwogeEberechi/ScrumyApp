7210c6b706a3ccb54990239404021bb0db2eabfa
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2018-04-26 15:19:27 +0100

    configure routes for adding usersand viewing all users

--- scrumy/scrumyapp/urls.py
+++ scrumy/scrumyapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('goals/', views.goals, name='goals'),
     path('<int:task_id>/', views.view_task, name='move'),
+    path('users/', views.get_users, name='users'),
+    path('adduser/', views.add_user, name='add_user'),
 ]
