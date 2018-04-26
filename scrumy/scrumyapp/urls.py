2ea350e09ddac902b234a4b39fe436d087b6c461
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2018-04-26 16:01:42 +0100

    use function names that point to their action

--- scrumy/scrumyapp/urls.py
+++ scrumy/scrumyapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('goals/', views.goals, name='goals'),
+    path('dailytask/', views.dailytask_goals, name='dailytask'),
     path('<int:task_id>/', views.move_goal, name='move_goal'),
     path('users/', views.get_users, name='users'),
     path('adduser/', views.add_user, name='add_user'),
--- scrumy/scrumyapp/views.py
+++ scrumy/scrumyapp/views.py
@@ -8,17 +8,17 @@ def index(request):
 	context = {'goals': goals}
 	return render(request, 'scrumyapp/index.html', context)
 
-def goals(request):
+def dailytask_goals(request):
 	statusDT = GoalStatus.objects.get(status='DT')
 	goals = statusDT.scrumygoals_set.all()
 	context = {'goals': goals}
-	return render(request, 'scrumyapp/goals.html', context)
+	return render(request, 'scrumyapp/dailytask.html', context)
 
 def move_goal(request, task_id):
 	goals = ScrumyGoals.objects.filter(task_id=task_id)
 
 	context = {'goals':goals, 'task_id':task_id}
-	return render(request, 'scrumyapp/dailytask.html', context)
+	return render(request, 'scrumyapp/goals.html', context)
 
 def add_user(request):
 	if request.method == "POST":
