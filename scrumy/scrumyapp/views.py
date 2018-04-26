feeb4ed487e2e55c5ba073440b943f945b9a4968
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2018-04-26 15:21:00 +0100

    add users view

--- scrumy/scrumyapp/views.py
+++ scrumy/scrumyapp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import ScrumyGoals, GoalStatus
+from django.shortcuts import render, redirect
+from .models import ScrumyGoals, GoalStatus, ScrumyUser
+from .forms import AddUserForm
 
 # Create your views here.
 def index(request):
@@ -12,10 +12,26 @@ def goals(request):
 	statusDT = GoalStatus.objects.get(status='DT')
 	goals = statusDT.scrumygoals_set.all()
 	context = {'goals': goals}
-	#goals = ScrumyGoals.objects.filter(status_id=4)
 	return render(request, 'scrumyapp/goals.html', context)
 
 def view_task(request, task_id):
 	goals = ScrumyGoals.objects.filter(task_id=task_id)
+
 	context = {'goals':goals, 'task_id':task_id}
 	return render(request, 'scrumyapp/dailytask.html', context)
+
+def add_user(request):
+	if request.method == "POST":
+		form = AddUserForm(request.POST)
+		if form.is_valid:
+			form.save()
+			return redirect('users')
+	else:
+		form = AddUserForm()
+	context = {'form': form}
+	return render(request, 'scrumyapp/adduser.html', context)
+
+def get_users(request):
+	users = ScrumyUser.objects.all()
+	context= {'users': users}
+	return render(request, 'scrumyapp/users.html', context)
