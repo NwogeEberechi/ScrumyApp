d044f66c9cf457e05262131d7c848acb8631febc
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2018-05-01 00:48:54 +0100

    add route for login

--- scrumy/scrumy/urls.py
+++ scrumy/scrumy/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('scrumy/', include('scrumyapp.urls') ),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
