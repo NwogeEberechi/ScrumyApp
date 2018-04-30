7558f52a73bed6b0179e49a99d1cf2e67db9703d
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2018-05-01 00:50:18 +0100

    fetch and display a given user and associated weekly task

--- scrumy/scrumyapp/views.py
+++ scrumy/scrumyapp/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
+from django.http import Http404
 from .models import ScrumyGoals, GoalStatus, ScrumyUser
 from .forms import AddUserForm
 
 # Create your views here.
 def index(request):
 	goals = ScrumyGoals.objects.all()
-	context = {'goals': goals}
+	user = ScrumyUser.objects.get(id=1)
+	statusWT = GoalStatus.objects.get(status='WT')
+	user_goals = statusWT.scrumygoals_set.filter(user_id=1)
+	context = {'goals': goals, 'user': user, 'user_goals':user_goals}
 	return render(request, 'scrumyapp/index.html', context)
 
 def dailytask_goals(request):
@@ -15,9 +19,11 @@ def dailytask_goals(request):
 	return render(request, 'scrumyapp/dailytask.html', context)
 
 def move_goal(request, task_id):
-	goals = ScrumyGoals.objects.filter(task_id=task_id)
-
-	context = {'goals':goals, 'task_id':task_id}
+	try:
+		goals = ScrumyGoals.objects.get(task_id=task_id)
+	except ScrumyGoals.DoesNotExist:
+		raise Http404('There is no goal with the task_id ' + str(task_id))
+	context = {'goals':goals, 'task_id':task_id}	
 	return render(request, 'scrumyapp/goals.html', context)
 
 def add_user(request):
