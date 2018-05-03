551d855e7f755dd17afa68781ff9ca0873188fc6
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2018-05-03 15:40:28 +0100

    add namespace to the scrumy url

--- scrumy/scrumyapp/urls.py
+++ scrumy/scrumyapp/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'scrumy'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('dailytask/', views.dailytask_goals, name='dailytask'),
     path('<int:task_id>/', views.move_goal, name='move_goal'),
     path('users/', views.get_users, name='users'),
     path('adduser/', views.add_user, name='add_user'),
+    path('addtask/', views.add_task, name='add_task'),
 ]
