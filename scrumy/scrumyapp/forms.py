9657cde7d18ab85bbe43dcc34cee18f1d266aa81
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2018-05-03 15:38:53 +0100

    create form for adding tasks

--- scrumy/scrumyapp/forms.py
+++ scrumy/scrumyapp/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import ScrumyUser
+from .models import ScrumyUser, ScrumyGoals
 
 class AddUserForm(forms.ModelForm):
     class Meta:
         model = ScrumyUser
         #fields = ['firstname', 'lastname', 'email', 'password', 'role']
+        fields = '__all__'
+
+class AddTaskForm(forms.ModelForm):
+    class Meta:
+        model = ScrumyGoals
         fields = '__all__'
