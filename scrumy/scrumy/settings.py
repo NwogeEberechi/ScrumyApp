ce2ca1789b8e7dfb147b7f970959aabc08221caf
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2018-05-03 11:53:58 +0100

    set login redirect path

--- scrumy/scrumy/settings.py
+++ scrumy/scrumy/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'scrumy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/scrumy/'
