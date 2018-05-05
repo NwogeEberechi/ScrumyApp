f896999a317eb7d0387f7642e030f07fb7077c59
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2018-05-05 12:43:03 +0100

    add change task status view

--- scrumy/scrumyapp/views.py
+++ scrumy/scrumyapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from .models import ScrumyGoals, GoalStatus, ScrumyUser
-from .forms import AddUserForm, AddTaskForm
+from .forms import AddUserForm, AddTaskForm, ChangeTaskStatusForm
 
 # Create your views here.
 def index(request):
@@ -49,3 +51,36 @@ def get_users(request):
 	users = ScrumyUser.objects.all()
 	context= {'users': users}
 	return render(request, 'scrumyapp/users.html', context)
+
+def change_task_status(request, goal_id):
+	if request.method =="POST":
+		form = ChangeTaskStatusForm(request.POST)
+		if form.is_valid:
+			new_status = request.POST.get('status_id')
+			status = GoalStatus.objects.get(id=new_status)
+			try:
+				goal = ScrumyGoals.objects.get(id=goal_id)
+			except ScrumyGoals.DoesNotExist:
+				raise Http404('There is no goal with the id ' + str(goal_id))
+			goal.status_id = status
+			# update moved_by field, but this require getting logged in user info
+			goal.save()
+			return redirect('scrumy:index')
+	else:
+		form = ChangeTaskStatusForm()
+	context = {'form': form}
+	return render(request, 'scrumyapp/changestatus.html', context)
+'''
+def login_user(request):
+	if request.method =='POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('scrumy:index')
+		else:
+			messages.error(request, 'Incorrect username or password')
+	context = {}
+	return render(request, 'registration/login.html', context)
+'''
